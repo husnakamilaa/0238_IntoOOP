72c7a95b622aa6096798d6c636d45e207ba9c581
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-04-23 15:55:01 +0700

    membuat methode procedure display

--- classbangundatar.cpp
+++ classbangundatar.cpp
@@ -18,4 +18,10 @@ class bangunDatar {
         float hitungLuas() {
             return panjang * Lebar;
         }
+
+        void display() {
+            cout << "Panjangnya = " << panjang << endl;
+            cout << "Lebarnya = " << Lebar << endl;
+            cout << "Luasnya = " << hitungLuas() << endl;
+        }
 };
